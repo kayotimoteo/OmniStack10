ad5ad2b25162bc1da685b1d77212100ed2d1da70
Kayo Timoteo <timoteokayo@gmail.com>
2020-02-25 23:16:53 -0400

    Initial Commit

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('', {
+  useNewUrlParser: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
