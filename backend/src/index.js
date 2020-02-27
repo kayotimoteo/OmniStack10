d7d570902e2ef38f6251d29cd8c348181fa256ae
Kayo Timoteo <timoteokayo@gmail.com>
2020-02-27 18:11:02 -0400

    add config env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv').config()
 const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
 const routes = require('./routes');
-const { setupWebsocket } = require('./websocket')
-
+const { setupWebsocket } = require('./websocket');
 const app = express();
 const server = http.Server(app);
-
 setupWebsocket(server);
 
-mongoose.connect('', {
-  useNewUrlParser: true
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use(cors());
